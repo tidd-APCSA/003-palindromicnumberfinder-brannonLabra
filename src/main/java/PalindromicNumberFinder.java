b69db202f31f36203cd954bde099881961352303
Brannon Labra <brannonalabra@gmail.com>
2020-11-13 06:03:20 +0000

    added searchForPalindromicNum and testPalindromicNum

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -13,12 +13,28 @@ public class PalindromicNumberFinder {
 
     // this method should find the next palindromic number
     public int searchForPalindromicNum(int num){
-
+      Integer firstInt = num;
+      String stringOne = firstInt.toString();
+      boolean flag = true;
+      while(flag){
+        firstInt++;
+        if(palindromicNumTest(firstInt)){
+          flag = false;
+        }
+      }
+      return firstInt - num;
     }
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
     public boolean testPalindromicNum(int num){
-
+      String s = reversNum(num);
+      int num1 = Integer.parseInt(s);
+      if(num1 == num){
+        return true;
+      }else{
+        return false;
+      }
+    
 
     }
 
