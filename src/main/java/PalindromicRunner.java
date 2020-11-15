e83be8e5c644430da61fcb7af0604a0b91a46b2d
Brannon Labra <brannonalabra@gmail.com>
2020-11-15 04:20:22 +0000

    removed space from reverse num

--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,6 +1,7 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
-      
+      PalindromicNumberFinder num1 = new PalindromicNumberFinder(70007);
 
+      System.out.println(num1);
     }
 }
